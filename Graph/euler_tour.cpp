0e8e459ac169dad64b7686fcd4145e44f58e07f4
Mah.D <125675444+Mahdi0203@users.noreply.github.com>
2025-02-17 10:59:45 +0600

    Add files via upload

---
+++ Graph/euler_tour.cpp
@@ -0,0 +1,22 @@
+ll timer, inTime[N], outTime[N];
+vector<ll> flatten;
+void euler_tour(int root, int par){
+  inTime[root] = timer++;
+  flatten.push_back(root);
+  for(auto child : g[root]){
+    if ( child != par ){
+      euler_tour(child, root);
+    }
+  }
+  outTime[root] = timer;
+}
+
+int main(){
+  // code
+
+  timer = 1;
+  flatten.push_back(0);
+  euler_tour(1, -1);
+
+  // code
+}
