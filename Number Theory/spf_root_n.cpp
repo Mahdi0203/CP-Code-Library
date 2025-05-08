e78e47ec9674fe020b7710009a8eb229d2d426c2
Mah.D <125675444+Mahdi0203@users.noreply.github.com>
2025-05-08 11:15:52 +0600

    Update spf_root_n.cpp

--- Number Theory/spf_root_n.cpp
+++ Number Theory/spf_root_n.cpp
@@ -1,13 +1,13 @@
 vector<int> (int n){	// sqrt(n)
-	vector<int> v;
-	for(int i = 2; i * i <= n; i++){
-		if(n % i == 0){
-			while(n % i == 0){
-				v.push_back(i);
-				n /= i;
-			}
-		}
-	}
-	if(n > 1) {v.push_back(n);}
-	return v;
+  vector<int> v;
+  for(int i = 2; i * i <= n; i++){
+    if(n % i == 0){
+      while(n % i == 0){
+	v.push_back(i);
+	n /= i;
+      }
+    }
+  }
+  if(n > 1) {v.push_back(n);}
+  return v;
 }
