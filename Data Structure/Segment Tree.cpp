6aa282326d10e377bacce816cb42c205b1182b85
Mah.D <125675444+Mahdi0203@users.noreply.github.com>
2026-02-19 02:49:00 +0600

    Update Segment Tree.cpp

--- Data Structure/Segment Tree.cpp
+++ Data Structure/Segment Tree.cpp
@@ -5,7 +5,7 @@ struct st {
   st(){
     memset( segT, 0, sizeof(segT) );
   }
-  inline void pull(int node, int lc, int rc){     
+  inline void pull(int node, int lc, int rc){    // change this function according to what you need 
     segT[node] = max(segT[lc], segT[rc]);
   }
   void build(int node, int b, int e){
@@ -22,7 +22,7 @@ struct st {
   void upd(int node, int b, int e, int i, int val){
     if( b > i || e < i ) {return;}
     if( b == e && e == i ){
-      segT[node] += val;
+      segT[node] += val;      // set according to problem
       return;
     }
     int mid = (b + e) >> 1;
@@ -32,7 +32,7 @@ struct st {
     pull( node, lc, rc );
   }
   ll query(int node, int b, int e, int i, int j){
-    if( b > j || e < i ) {return -inf;}      
+    if( b > j || e < i ) {return -inf;}      // return a neutral value according to problem
     if( b >= i && e <= j ) {return segT[node];}
     int mid = (b + e) >> 1;
     int lc = (node << 1), rc = (node << 1) + 1;
@@ -40,4 +40,5 @@ struct st {
     ll R = query(rc, mid + 1, e, i, j);
     return max(L, R);       
   }
+
 } segT;
